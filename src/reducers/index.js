a6b4a25cf2e2473e57951df81b5f4dbec0112c48
Paton Vinal <paton.vinal@gmail.com>
2017-06-26 16:07:40 -0400

    finished reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,6 +9,44 @@ const initialState ={
 
 
 export const hotColdReducer = (state=initialState, action) => {
-    
+    if(action.type === actions.NEW_GAME) {
+        return Object.assign({}, initialState, {
+            correctAnswer: action.correctAnswer
+        })
+    }
+    else if(action.type === actions.TOGGLE_INFO_MODAL) {
+        return Object.assign({}, state, {
+            showInfoModal: !state.showInfoModal
+        });
+    } else if(action.type === actions.GUESS) {
+        const guess = parseInt(action.guess, 10);
+        if(isNaN(guess)) {
+            return Object.assign({}, state,{
+                feedback: 'Please enter a valid number'
+            });
+        }
+        const difference = Math.abs(guess - state.correctAnswer);
+        let feedback;
+        if (difference >= 50) {
+            feedback = 'You\'re Ice Cold...';
+        }
+        else if (difference >= 30) {
+            feedback = 'You\'re Cold...';
+        }
+        else if (difference >= 10) {
+            feedback = 'You\'re Warm';
+        }
+        else if (difference >= 1) {
+            feedback = 'You\'re Hot!';
+        }
+        else {
+            feedback = 'You got it!';
+        }
+        return Object.assign({}, state, {
+            guesses: [...state.guesses, guess],
+            feedback
+        });
+    }
+    return state;
 }
 
